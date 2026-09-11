139e4e6417b1aa63381b498e5833c6888dbbe102
Gusev Vladimir <162140064+Gusev-V@users.noreply.github.com>
2026-09-11 16:46:27 +0300

    Исправление реализации кэша

--- source/factorial.js
+++ source/factorial.js
@@ -14,7 +14,6 @@
  * factorial(-1); 
  */
 let factorialCache = [1];
-let factorialCacheSize = 1;
 
 const MAX_CACHE_SIZE = 50;
 
@@ -37,50 +36,18 @@ const factorial = function(num) {
         return factorialCache[num];
     }
 
-    // Поиск ближайщего уже вычесленного факториала из кэша
-    let leftNum = num - 1;
-    let rightrNum = num + 1;
-    let nearestNum = 0;
-    let foundRight = false;
-
-    while (leftNum >= 0 || rightrNum < factorialCache.length) {
-        if (factorialCache[leftNum] !== undefined){
-            nearestNum = leftNum;
-            break;
-        }
-        if (factorialCache[rightrNum] !== undefined){
-            nearestNum = rightrNum;
-            foundRight = true;
-            break;
-        }
-        leftNum--;
-        rightrNum++;
-    }
-    
-    let result = factorialCache[nearestNum];
-    // Если ближайщий существующий факториал находится справа, то делим до нужного
-    if (foundRight){
-        for (let i = nearestNum; i >= num + 1; i--) {
-            // round используется для устранения артифактов деления, безопасно тк result / i обязан дать целочисленный ответ
-            result = Math.round(result / i);
-        }
-    }
-    // Если ближайщий существующий факториал находится слево, то умножаем до нужного
-    else {
-        for (let i = nearestNum + 1; i <= num; i++) {
-            result *= i;
-        }
-    }
+    // Берем последний вычисленный факториал
+    let result = factorialCache[factorialCache.length - 1];
+    for (let i = factorialCache.length; i <= num; i++) {
+        result *= i;
+        // Записываем в кэш вычисленное значение
+        factorialCache[i] = result;
+    }    
 
     // Сбрасываем кэш перед добавлением нового значения, если достигнут лимит
-    if (factorialCacheSize >= MAX_CACHE_SIZE) {
+    if (factorialCache.length >= MAX_CACHE_SIZE) {
         factorialCache = [1];
-        factorialCacheSize = 1;
     }
-
-    // Записываем в кэш вычисленное значение
-    factorialCache[num] = result;
-    factorialCacheSize++;
     
     return result;
 };
